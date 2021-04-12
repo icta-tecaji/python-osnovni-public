385d9e2f46aae45656c72501423c40b273bccb0f
Balki <balkovec.gregor@gmail.com>
2021-04-12 20:10:31 +0200

    Dodano predavanje Importing,WebScraping. Dodana DN

---
+++ 10_Importing_WebScraping/DN/DN_Termin_08_CLEAN.py
@@ -0,0 +1,69 @@
+'''
+Naloga: 
+Ustvarite skripto, ki nam pove v kateri fazi COVID semaforja se nahajamo glede na Št. pozitivnih testov - 7dnevno povprečje in število hospitaliziranih.
+Podatke pridobite na https://api.sledilnik.org/api/stats. Podani so v JSON formatu, z najnovejšimi podatki na koncu.
+'''
+
+# Rešitev
+
+
+
+
+'''
+Naloga: 
+Ustvarite skripto, ki nam pridobi podatke slovenski občin na spletni strani: https://skupnostobcin.si/podatki/zemljevid-obcin-tabela/#p2
+Skripta naj ustvari nov dictionary v katerega shrani Ime občine, število prebivalcev, površino (km2) in število prebivalca/km2 - ni potrebno upoštevati decimalk.
+
+data {
+    "Ljubljana": {
+        "Št. prebivalcev": 273091,
+        "Površina (km2)": 275,
+        "Št.preb/km2": 993
+        },
+    "Piran": {
+        "Št. prebivalcev": 17268,
+        "Površina (km2)": 44.6,
+        "Št.preb/km2": 387
+        },
+    ...
+    }
+'''
+
+
+# Rešitev
+
+
+
+
+
+'''
+Naloga: 
+Ustvarite skripto, ki pridobi informacije o 250 najbolje ocenjenih filmih.
+https://www.imdb.com/chart/top/?ref_=nv_mv_250
+
+Skripta naj pridobi naslov filma, oceno filma in trajanje filma. Trajanje filma dobite, če odprete specifični film.
+
+Output:
+Kaznilnica odrešitve
+9.2
+2h 22min
+
+Boter
+9.1
+2h 55min
+
+Boter, II. del
+9.0
+3h 22min
+
+Vitez teme
+9.0
+2h 32min
+
+...
+''''
+
+# Rešitev
+
+
+
