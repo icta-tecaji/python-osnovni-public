3821128ad8461397269da08675e8192270127414
Gregor Balkovec <balkovec.gregor@gmail.com>
2021-11-03 19:58:56 +0100

    Predavanje04

---
+++ Termin_04 -/DN04/DN04_03.py
@@ -0,0 +1,15 @@
+'''
+Naloga: 
+Podan imate dataset učencev in njihovih ocen.
+S pomočjo list comprehensions ustvarite nov list v katerem so samo imena učencev, ki imajo oceno večjo kot 50.
+
+Primeri:
+
+Input:
+students = [ ["Ana", 55], ["Anže", 96], ["Andrej", 67], ["Bojan", 88], ["Črt", 100], ["Dajana", 49], ["Erika", 79], ["Francis", 11] ]
+
+Output:
+['Ana', 'Anže', 'Andrej', 'Bojan', 'Črt', 'Erika']
+'''
+#Rešitev
+
