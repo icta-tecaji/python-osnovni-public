8d56129d8705b0901f441f4b7f0c0a50442228ea
anzeglusic <62444755+anzeglusic@users.noreply.github.com>
2022-03-27 21:01:40 +0200

    Moving DN to Termin_06 folder

--- Termin_05/DN/06.py
+++
@@ -1,5 +0,0 @@
-"""
-Naloga:
-
-Napiši razred Vislice, ki bo hranila trenutna stanja (iskano besedo, ugibajoče črke, ugotovljene črke, ...) in funkcije za izvajanje igre "vislice". Poskusite narediti igro sami, če pa so težave, si pomagajte z že narejeno nalogo, ki smo jo naredili tekom predavanj. Pomembno je, da se celotna definicija igre nahaja znotraj razreda Vislice (torej vse potrebne spremenljivke in funkcije naj bodo del razreda). 
-"""
