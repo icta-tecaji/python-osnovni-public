927b01350eb80d4253684c4dd0c80188b76da4c6
Gregor Balkovec <balkovec.gregor@gmail.com>
2022-04-26 16:12:21 +0200

    Izpit dodan.

---
+++ Termin_10-Izpit/naloga1.py
@@ -0,0 +1,17 @@
+'''
+Naloga 01¶
+Točka: / 5
+
+Napišite funkcijo funkcija01, ki kot prvi parameter sprejme premer krogle. Funkcija naj vrne izračunano prostornino krogle.
+
+"π" najdete v built-in knjižnjici math.
+
+import math
+print(math.pi)
+
+INPUT:
+funkcija01(3)
+
+OUTPUT:
+14.1371
+'''
