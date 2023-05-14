78dcaaf5673e311bb52f0a9465070728447890f7
anzeglusic <anze.glusic@gmail.com>
2023-05-15 01:36:59 +0200

    Datoteke in pdf

---
+++ Termin04/02.py
@@ -0,0 +1,19 @@
+# pass
+
+def izpisi_leto_rojstva(temp):
+	pass
+
+
+# print(123)
+
+x = int(input("Vnesi svojo starost: "))
+starost = 2023-x
+izpisi_leto_rojstva(starost)
+
+
+
+if starost > 18:
+	print("Lahko kupi alkohol.")
+else:
+	pass # TODO: kaj narediti ko ne sme kupiti alkohola
+print("123")
