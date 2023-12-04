92274b7895f8ccc8227d4c92ac3aaca53d41a970
27kaste <n.kaste@gmail.com>
2023-12-04 20:49:30 +0100

    Dodan 8. termin

---
+++ Termin08/Primer_izpita/Naloga02.py
@@ -0,0 +1,15 @@
+# Naloga 02
+# Točke: / 5
+
+# Napišite funkcijo funkcija02, ki prejme list.
+
+# Vsak element v listu je tuple. Prvi element v tuplu je ocena, 
+# katero je študente prejel na izpitu. Drugi element je ime študenta.
+
+# Funkcija naj vrnte list imen študentov, ki so imeli oceno 50 ali več.
+
+# INPUT:
+# funkcija02([[50, "Jan"], [49, "Anže"], [74, "Tine"], [100, "Anja"], [12, "Zvone"], [81, "Mirko"]])
+
+# OUTPUT:
+# ['Jan', 'Tine', 'Anja', 'Mirko']
---
+++ Termin08/Primer_izpita/Naloga05.py
@@ -0,0 +1,3 @@
+#Definiraj razred trikotnik
+#Kot instanco definiraj 3 spremenljivke tipa integer, ki definirajo stopinje vsakega izmed kotov
+#Napiši funkcijo, ki preveri, če je seštevek vseh kotov enak 180stopinj
