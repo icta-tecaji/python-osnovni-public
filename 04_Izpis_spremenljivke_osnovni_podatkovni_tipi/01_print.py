a0f603258f7f088020f7a29d3c981789089d10cc
Leon Stefanic Juznic <leon.stefanic@ltfe.org>
2024-10-14 19:54:28 +0200

    dan 1

---
+++ 04_Izpis_spremenljivke_osnovni_podatkovni_tipi/01_print.py
@@ -0,0 +1,51 @@
+print("Hello World!\n")
+print(10)
+print(10.5)
+print("Danes", 4, "je", 5, "leto")
+
+# Izpis spremenljivk
+
+x = 2
+y = 3
+rezultat = x + y
+print(rezultat)
+
+number_of_items = 10
+
+# Podatkovni tipi
+# Integer
+x = 10
+y = -45
+x = 666666666666666666666666666666666666666666666666666666666666666
+print(type(x))
+
+# Float
+x = 10.5
+y = -45.5
+print(type(x))
+print(0.1)
+print(0.1 + 0.1 + 0.1 == 0.3)
+
+# String
+moj_string = "Hello World!"
+prazno_besedilo = ""
+print(type(moj_string))
+vecvrsticni_string = """Msmddededefefef
+
+                    efefefe
+
+
+fefeff"""
+print(vecvrsticni_string)
+
+# Boolean
+x = True
+y = False
+print(type(x))
+
+# None
+x = None
+print(type(x))
+
+# Input funkcija
+vstavljena_vrednost = input("Vnesi vrednost: ")
---
+++ 04_Izpis_spremenljivke_osnovni_podatkovni_tipi/02_primer.py
@@ -0,0 +1,11 @@
+"""Preprosti kalkulator za seštevanje dveh števil."""
+
+# Vnesemo prvo število
+prvo_stevilo = float(input("Vnesi prvo število: "))
+drugo_stevilo = float(input("Vnesi drugo število: "))
+
+# Izračunamo vsoto
+vsota = prvo_stevilo + drugo_stevilo
+
+# Izpišemo rezultat
+print("Vsota je:", vsota)
