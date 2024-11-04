05291bef1d8438f22ab34c168da88245d220dead
Leon Stefanic Juznic <leon.stefanic@ltfe.org>
2024-11-04 15:16:34 +0100

    add del 07

---
+++ 07_Objektno_programiranje/DN/DN04_02.py
@@ -0,0 +1,17 @@
+'''
+Napišite program, ki preveri ali je neka beseda palindrom.
+Palindrom je beseda, ki se bere enako naprej in nazaj. (taco cat  - palindrom, madam - palindrom, ...)
+
+Primeri:
+
+Input: taco cat
+Output: Beseda je palindrom
+
+Input: madam
+Output: beseda je palindrom
+
+Input: konjederec
+Output: beseda ni palindrom
+'''
+
+# Rešitev
---
+++ 07_Objektno_programiranje/Naloga_01.py
@@ -0,0 +1,16 @@
+# NALOGA 1
+# ustvarite razred Vozilo. Vsaka instanca naj ima svojo specifično hitrost in kolometrino.
+# Max hitrost vozila: 200 km/h. Prevozenih je 100000 km.
+# naredite en avto in en motor
+
+# NALOGA 2
+# vsaka instanca naj ima sedaj poled podatka o hitrosti in kilometrini se podatek o porabljenem gorivu
+# razred vozilo naj ima funkcijo poraba(), ki vrne(RETURN) povprecno porabo vozila
+
+# NALOGA 3
+# dodaj razredno spremenljivko razredu vozilo
+# spremenljivki naj bo ime st_koles = 4
+# dodajte metodo opis, ki naj izpise opis vozila
+# ustvarite podrazreda avto in motor
+# razreda naj dedujete od razreda Vozila
+# Razred Motor naj ima stevilo gum enako 2
