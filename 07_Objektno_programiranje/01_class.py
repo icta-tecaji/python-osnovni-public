767d3a894225362fa127c974d7e640a4c1b7ffb8
Luka Levac <luka.levac@gmail.com>
2024-11-21 22:05:42 +0100

    komentarji prvi del classa

--- 07_Objektno_programiranje/01_class.py
+++ 07_Objektno_programiranje/01_class.py
@@ -1,4 +1,25 @@
+"""V skripti je malo mešanja slovenščine in angleščine. Načeloma je dobra praksa, da se uporablja
+angleščino.
+"""
+
+
 class Pes:
+    """Class Pes represents a dog with attributes for species, favorite food, name, and age.
+
+    Attributes:
+        vrsta (str): The species of the animal, default is "sesalec".
+        najljubsa_hrana (str): The favorite food of the dog, default is "briketi".
+        ime (str): The name of the dog.
+        starost (int): The age of the dog.
+
+    Methods:
+        povej_mi_starost():
+            Prints the age of the dog in a formatted string.
+        spremeni_najbljubso_hrano(hrana):
+            Changes the favorite food of the dog to the provided food.
+
+    """
+
     vrsta = "sesalec"
     najljubsa_hrana = "briketi"
 
@@ -9,7 +30,7 @@ class Pes:
     def povej_mi_starost(self):
         print(f"{self.ime} je star {self.starost} let")
 
-    def spremeni_najbljubso_hrano(self, hrana):
+    def spremeni_najljubso_hrano(self, hrana):
         self.najljubsa_hrana = hrana
 
 
@@ -22,7 +43,7 @@ print(f"{rex.ime} je naraje: {rex.najljubsa_hrana}")
 print(f"{joli.ime} je naraje: {joli.najljubsa_hrana}")
 print(f"{fido.ime} je naraje: {fido.najljubsa_hrana}")
 
-rex.spremeni_najbljubso_hrano("konzerva")
+rex.spremeni_najljubso_hrano("konzerva")
 
 print(f"{rex.ime} je naraje: {rex.najljubsa_hrana}")
 print(f"{joli.ime} je naraje: {joli.najljubsa_hrana}")
