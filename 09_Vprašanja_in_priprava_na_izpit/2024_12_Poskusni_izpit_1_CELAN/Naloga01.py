8a48284d254b1b7b009cdb3b73f49da7ab0f9fb3
anzeglusic <anze.glusic@gmail.com>
2024-12-09 19:02:56 +0100

    zaključek: vprašanja + priprava na izpit

--- 09_Vprašanja_in_priprava_na_izpit/2024_12_Poskusni_izpit_1_CELAN/Naloga01.py
+++ 09_Vprašanja_in_priprava_na_izpit/2024_12_Poskusni_izpit_1_CELAN/Naloga01.py
@@ -1,9 +1,9 @@
 # Naloga 01
 # Točka: / 5
 
-# Napišite funkcijo funkcija01, ki kot prvi parameter sprejme višino valja in 
-# kot drugi parameter sprejme radij valja. Funkcija naj vrne izračunano 
-# prostornino valja (lahko privzamete, da so vse volumenske enote v 
+# Napišite funkcijo funkcija01, ki kot prvi parameter sprejme višino valja in
+# kot drugi parameter sprejme radij valja. Funkcija naj vrne izračunano
+# prostornino valja (lahko privzamete, da so vse volumenske enote v
 # centimetrih in da je izhod v kubičnih centimetrih).
 
 # "π" najdete v built-in knjižnjici math.
--- 09_Vprašanja_in_priprava_na_izpit/2024_12_Poskusni_izpit_2_CLEAN/Izpit_Naloga05_CLEAN.py
+++ 09_Vprašanja_in_priprava_na_izpit/2024_12_Poskusni_izpit_2_CLEAN/Izpit_Naloga05_CLEAN.py
@@ -1,5 +1,4 @@
-'''
-Naloga 05
+"""Naloga 05
 Točke: /5
 
 Napišite funkcijo funkcija05, ki prejme kot argument dictionary. Ključ:vrednost sta oblike integer:list. Vaša naloga je, da iz vsakega lista pričnete izločati maximalne vrednosti, dokler celotni seštevek lista ni manjši od njegovega ključa. Posodobljeni dictionary vrnite.
@@ -13,30 +12,27 @@ funkcija05(x)
 
 Naj vrne dictionary v obliki:
 {
-    12: [1, 5, 3], 
-    45: [3, 12, 3, 5], 
+    12: [1, 5, 3],
+    45: [3, 12, 3, 5],
     23: [3, 6, 8]
 }
-'''
+"""
 
 # Rešitev
 
-
-
-
-
-
 if __name__ == "__main__":
     x = {
-        12: [1,5,12,46,3,24,9],
-        45: [3,43,23,67,12,3,5],
-        23: [64,12,34,46,86,21,3,6,8]
+        12: [1, 5, 12, 46, 3, 24, 9],
+        45: [3, 43, 23, 67, 12, 3, 5],
+        23: [64, 12, 34, 46, 86, 21, 3, 6, 8],
     }
 
     rezultat = funkcija05(x)
     correct = True
     if len(rezultat.keys()) != 3:
-        print(f"Primer ni vredu!!! Rezultat ima preveč ključev {len(rezultat.keys())}: {rezultat.keys()}. Pričakuje se, da ima 3: 12, 45 in 23.")
+        print(
+            f"Primer ni vredu!!! Rezultat ima preveč ključev {len(rezultat.keys())}: {rezultat.keys()}. Pričakuje se, da ima 3: 12, 45 in 23.",
+        )
         correct = False
     if rezultat[12] != [1, 5, 3]:
         print(f"Primer ni vredu!!! Za rezultat se je dobilo {rezultat[12]}. Pričakovalo se je [1, 5, 3]")
