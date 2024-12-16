0962a8a8b5b0b04ea2ee6bbb858466deb38b9917
anzeglusic <anze.glusic@gmail.com>
2024-12-16 16:16:35 +0100

    Izpit

---
+++ 2024_12_Izpit_CLEAN/Naloga01.py
@@ -0,0 +1,18 @@
+# Naloga 01
+# **Točka: / 5**
+
+# Napišite funkcijo `funkcija01`, ki kot prvi parameter sprejme premer krogle. Funkcija naj vrne izračunano prostornino krogle na 2 decimalki natančno.
+#Formula za izračun je V = 4/3 * π * (d/2)^3
+
+# "π" najdete v built-in knjižnjici **math**.
+
+# ```python
+# import math
+# print(math.pi)
+
+# INPUT:
+# funkcija01(3)
+
+# OUTPUT:
+# 14.1371
+# ```
---
+++ 2024_12_Izpit_CLEAN/Naloga04.py
@@ -0,0 +1,27 @@
+# # Naloga 04
+# **Točke: /5**
+
+# Napišite funkcijo `funkcija04`. Funkcija naj odpre datoteko `input.txt` v kateri imamo napisan recept za peko palačink vendar pa so koraki v napačnem zaporedju.
+
+# Funkcija na korake razporedi v pravilni zaporedje in jih nato zapiše v datoteko `output.txt`.
+
+# Lahko privzamete, da bo številka koraka vedno zavzela prvi dve mesti vrstice.
+
+# ```
+# INPUT:
+# 03. Segrejte ponev in nanjo vlijte olje.
+# 01. Zmešajte 0.5l mleka, 2 jajci in 1 žlico olja.
+# 04. Na ponev zlijte 1 zajemalko mase.
+# 06. Palačinko namažite s čokolado ali pa marmelado in kislo smetano.
+# 05. Palačinko popecite na obeh straneh, da pridobi zlato-rjavo barvo.
+# 02. Masi dodajte 25dkg bele moke in sol.
+
+
+# OUTPUT:
+# 01. Zmešajte 0.5l mleka, 2 jajci in 1 žlico olja.
+# 02. Masi dodajte 25dkg bele moke in sol.
+# 03. Segrejte ponev in nanjo vlijte olje.
+# 04. Na ponev zlijte 1 zajemalko mase.
+# 05. Palačinko popecite na obeh straneh, da pridobi zlato-rjavo barvo.
+# 06. Palačinko namažite s čokolado ali pa marmelado in kislo smetano.
+# ```
